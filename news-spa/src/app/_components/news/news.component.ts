a142a83f9166b2106ec4cdbf2b1e18a1149d8660
yoandryLumpuy <yoandry@atiss.une.cu>
2021-02-21 10:23:10 -0500

    ng build --prod hizo la compilacion en la carpeta wwwroot del proyecto dotnet api.

--- news-spa/src/app/_components/news/news.component.ts
+++ news-spa/src/app/_components/news/news.component.ts
@@ -1,3 +1,5 @@
+import { AuthService } from 'src/app/_services/auth.service';
+import { Router } from '@angular/router';
 import { BannerStructureService } from './../../_services/banner-structure.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor(private bannerStructureService: BannerStructureService) { }
+  constructor(private bannerStructureService: BannerStructureService,
+    private router: Router,
+    private authService: AuthService) { }
 
   ngOnInit() {
     this.bannerStructureService.updateBanner({centeredText: 'News'});
+    if (!this.authService.loggedIn) this.router.navigate(['login']);
   }
 
 }
